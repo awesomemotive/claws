d403d15e7de27d9d9d803113562cfca49756208a
DrewAPicture <info@drewapicture.com>
2017-07-06 12:36:15 -0600

    Stub out the \Sandhills\Sidecar class and helper.

---
+++ sidecar.php
@@ -0,0 +1,61 @@
+<?php
+namespace Sandhills {
+
+	/**
+	 * Standalone library to provide a variety of database sanitization helpers when
+	 * interacting with WordPress' wp-db class for custom queries.
+	 *
+	 * @since 1.0.0
+	 */
+	class Sidecar {
+
+		/**
+		 * The single Sidecar instance.
+		 *
+		 * @access private
+		 * @since  1.0.0
+		 * @var    \Sandhills\Sidecar
+		 * @static
+		 */
+		private static $instance;
+
+		/**
+		 * Sidecar version.
+		 *
+		 * @access private
+		 * @since  1.0.0
+		 * @var    string
+		 */
+		private $version = '1.0.0';
+
+		/**
+		 * Sets up and retrieves the Sidecar instance.
+		 *
+		 * @access public
+		 * @since  1.0.0
+		 * @static
+		 *
+		 * @return \Sandhills\Sidecar
+		 */
+		public static function instance() {
+
+			if ( ! isset( self::$instance ) && ! ( self::$instance instanceof Sidecar ) ) {
+				self::$instance = new Sidecar;
+			}
+
+			return self::$instance;
+		}
+
+	}
+}
+
+/**
+ * Shorthand helper for retrieving the Sidecar instance.
+ *
+ * @since 1.0.0
+ *
+ * @return \Sandhills\Sidecar Sidecar instance.
+ */
+function sidecar() {
+	return \Sandhills\Sidecar::instance();
+}
